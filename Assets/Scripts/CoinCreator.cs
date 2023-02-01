bc06e7b4bf33efedf68059c7dd15f4dbae5d5d6d
wedgon6 <117192830+wedgon6@users.noreply.github.com>
2023-02-01 20:21:37 +0300

    first

---
+++ Assets/Scripts/CoinCreator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCreator : MonoBehaviour
+{
+    [SerializeField] private Coin _coin;
+    [SerializeField] private Transform _point;
+
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = new Transform[_point.childCount];
+
+        for (int i = 0; i < _point.childCount; i++)
+        {
+            _points[i] = _point.GetChild(i);
+        }
+
+        StartCoroutine(Create());
+    }
+
+    private IEnumerator Create()
+    {
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_coin, _points[i].position, Quaternion.identity);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float _speed = 2;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    private float _jumpForse = 5;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0,0);
+            _animator.SetTrigger("MoveRight");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            _animator.SetTrigger("MoveLeft");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // _rigidbody2D.AddForce(Vector2.up*_jumpForse);
+            transform.Translate(0, _speed * Time.deltaTime*_jumpForse, 0);
+        }
+    }
+}
